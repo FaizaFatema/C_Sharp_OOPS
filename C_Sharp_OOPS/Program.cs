95c6428b014ddd771e34886d7f3acc8a6a31f44e
Faiza Fatema <faiza.fatema.1947@gmail.com>
2023-02-23 17:37:41 +0530

    create initial project in visual studio

---
+++ C_Sharp_OOPS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_OOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
